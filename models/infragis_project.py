b77dd1266eedbc289c892340d5a25bfedd7fe638
Tom Palan <thomas@palan.at>
2020-10-27 15:12:25 +0100

    add introduction date and link to initial invoice

--- models/infragis_project.py
+++ models/infragis_project.py
@@ -3,10 +3,11 @@ from odoo import models, fields, api
 class InfragisProject(models.Model):
     _name = 'infragis.project'
     _description = 'IGIS Projekt'
-    name = fields.Char(required=True)
-    end_customer_id = fields.Many2one('res.partner', string="Endkunde", required=True)
+    partner_id = fields.Many2one('res.partner', string="Kunde", required=True)
     alt_customer_id = fields.Many2one('res.partner', string="Alt. Re-Empfänger")
     customer_id = fields.Many2one('res.partner', string="Re-Empfänger", readonly=True, store=True, compute='_compute_customer_id')
+    introduction_date = fields.Date(string='Einschulung')
+    initial_invoice_id = fields.Many2one('account.move', string="Initiale Rechnung")
 
     @api.depends('alt_customer_id')
     def _compute_customer_id(self):
@@ -14,5 +15,5 @@ class InfragisProject(models.Model):
             if project.alt_customer_id:
                 project.customer_id = project.alt_customer_id
             else:
-                project.customer_id = project.end_customer_id
+                project.customer_id = project.partner_id
 
