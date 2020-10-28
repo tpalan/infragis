560bbac00045ae5749244bc4e3ea2671fbdd9478
Tom Palan <thomas@palan.at>
2020-10-28 18:47:09 +0100

    add proper currency handling add mail.thread handling

--- models/assessment_basis.py
+++ models/assessment_basis.py
@@ -5,17 +5,14 @@ class AssessmentIndex(models.Model):
     _name = 'assessment.index'
     _description = 'Index'
 
-    name = fields.Integer('Jahr', required=True) # TODO: format without thousand seperator
-    currency_id = fields.Many2one('res.currency', string="Currency", compute='_get_default_currency',
+    name = fields.Integer('Jahr', required=True)  # TODO: format without thousand seperator
+    currency_id = fields.Many2one('res.currency', string="Currency",
+                                  default=lambda self: self.env.user.company_id.currency_id,
                                   help='Utility field to express amount currency', store=True, readonly=True)
     value = fields.Monetary('Wert', currency_field='currency_id', required=True)
 
-    def _get_default_currency(self):
-        for index in self:
-            index.currency_id = self._context.get('default_currency_id')
-
     _sql_constraints = [
         ('unique_year',
-         'unique(year)',
+         'unique(name)',
          'Eintrag für Jahr existiert bereits')
     ]
--- models/infragis_project.py
+++ models/infragis_project.py
@@ -1,30 +1,59 @@
+import datetime
+from pprint import pprint
+
 from odoo import models, fields, api
 
 
 class InfragisProject(models.Model):
     _name = 'infragis.project'
     _description = 'IGIS Projekt'
+    _inherit = ['mail.thread']
+    _order = 'sale_order_accepted_date desc, id desc'
+    _mail_post_access = 'read'
 
     name = fields.Char(
         'Bezeichnung', compute='_compute_name', store=True)
 
     partner_id = fields.Many2one('res.partner', string="Kunde", required=True)
-    # alt_customer_id = fields.Many2one('res.partner', string="Alt. Re-Empfänger")
-    # customer_id = fields.Many2one('res.partner', string="Re-Empfänger", readonly=True, store=True, compute='_compute_customer_id')
+
     introduction_date = fields.Date(string='Einschulung')
-    initial_invoice_id = fields.Many2one('account.move', string="Initiale Rechnung")
+    initial_invoice_id = fields.Many2one('account.move', string="Rechnung Kauf")
 
-    recurring_invoice_start_date = fields.Date(string="Wartungsgebühr ab")
+    recurring_invoice_start_date = fields.Date(string="Wartungsgebühr ab", tracking=True)
 
-    commission_partner_id = fields.Many2one('res.partner', string="Provisions-Kunde")
+    sale_order_accepted_date = fields.Date(string="Angebot akzeptiert", tracking=True)
+    sale_order_sent_date = fields.Date(string="Angebot verschickt", tracking=True)
 
-    sale_order_id = fields.Many2one('sale.order', string="Angebot")
-    sale_order_line_ids = fields.Many2many('sale.order.line', string="Rechnungs-Positionen", readonly=True, store=True,
+    commission_partner_id = fields.Many2one('res.partner', string="Provisions-Empfänger")
+
+    sale_order_id = fields.Many2one('sale.order', string="Angebot", tracking=True)
+    sale_order_line_ids = fields.Many2many('sale.order.line', string="Gebühren", readonly=True, store=True,
                                            compute='_compute_sale_order_lines')
+
+    currency_id = fields.Many2one('res.currency', string="Currency",
+                                  default=lambda self: self.env.user.company_id.currency_id, store=True, readonly=True)
+    price_sum_total = fields.Monetary(string="Aktuelle Monatsrate", readonly=True, store=True,
+                                      compute='_compute_price_sum',
+                                      currency_field='currency_id')
+
     assessment_index_id = fields.Many2one('assessment.index', string="Berechnungsgrundlage", readonly=True,
                                           compute='_compute_assessment_index')
 
+    year = fields.Integer(string="Jahr", help="Dummy field for reacting to year changes", readonly=True,
+                          default=datetime.datetime.now().year)
+
     invoices = fields.Many2many('account.move', string="Rechnungen", readonly=True, compute='_compute_invoices')
+
+    # helper fields
+    project_user_id = fields.Many2one('res.users', copy=False, tracking=True,
+                                      string='Salesperson',
+                                      default=lambda self: self.env.user)
+    user_id = fields.Many2one(string='User', related='project_user_id',
+                              help='Technical field used to fit the generic behavior in mail templates.')
+
+    def _get_default_currency(self):
+        for index in self:
+            index.currency_id = self._context.get('default_currency_id')
 
     @api.depends('partner_id')
     def _compute_name(self):
@@ -35,8 +64,24 @@ class InfragisProject(models.Model):
     def _compute_invoices(self):
         for project in self:
             project.invoices = self.env['account.move'].search(
-                [('type', '=', 'out_invoice'),'|', ('end_customer_id', '=', project.partner_id.id), ('partner_id', '=', project.partner_id.id)])
+                [('type', '=', 'out_invoice'), '|', ('end_customer_id', '=', project.partner_id.id),
+                 ('partner_id', '=', project.partner_id.id)])
 
+    @api.depends('sale_order_line_ids', 'assessment_index_id', 'sale_order_line_ids.price_total', 'year')
+    def _compute_price_sum(self):
+        # get index for current year
+        # get current year
+        cur_year = datetime.datetime.now().year
+        cur_index = self.env['assessment.index'].search([('name', '=', cur_year)], limit=1)
+        for project in self:
+            # get our current index
+            project.price_sum_total = 0
+            if not project.assessment_index_id:  # we need the original index
+                continue
+            old_index_value = project.assessment_index_id.value
+
+            for line in project.sale_order_line_ids:
+                project.price_sum_total += line.price_subtotal / old_index_value * cur_index.value
 
     @api.depends('sale_order_id')
     def _compute_sale_order_lines(self):
@@ -44,8 +89,10 @@ class InfragisProject(models.Model):
             if project.sale_order_id:
                 project.sale_order_line_ids = project.sale_order_id.order_line.filtered(
                     lambda so: so.display_type not in ['line_section', 'line_note'])
+            else:
+                project.sale_order_line_ids = []
 
-    @api.depends('sale_order_id')
+    @api.depends('sale_order_id', 'sale_order_id.assessment_index_id')
     def _compute_assessment_index(self):
         for project in self:
             if project.sale_order_id:
