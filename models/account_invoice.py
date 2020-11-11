789be8a7b8d3af915aec72d1bbf5aba245657165
Tom Palan <thomas@palan.at>
2020-11-11 14:40:27 +0100

    add project link to invoice form

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -5,7 +5,7 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
     end_customer_id = fields.Many2one('res.partner', string="Endkunde", readonly=True,
                                       states={'draft': [('readonly', False)]})
-    igis_project_id = fields.Many2one('infragis.project')
+    igis_project_id = fields.Many2one('infragis.project', string="IGIS-Projekt")
     commission_partner_id = fields.Many2one('res.partner', string="Provisions-Kunde", related='igis_project_id.commission_partner_id')
 
 
